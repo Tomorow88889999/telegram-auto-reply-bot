57908aa6892d373c2778c828350386a95df8f8ea
Tomorow88889999 <tomorrowland919191@gmail.com>
2025-05-13 11:44:00 +0700

    main.py

--- main.py
+++ main.py
@@ -73,4 +73,8 @@ async def main():
     )
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    import nest_asyncio
+    nest_asyncio.apply()
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
